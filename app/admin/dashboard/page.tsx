2c13f669bbc985a4ee3366ed024f583a495908b9
Marseille93 <ibrahim13.dieng@gmail.com>
2025-08-30 10:06:30 +0000

    ajout prop admin/dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -121,8 +121,7 @@ export default function AdminDashboard() {
 
 	return (
 		<div className="min-h-screen bg-gray-50">
-			<Header />
-
+			<Header role="admin" />
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
 				{/* Dashboard Header */}
 				<div className="mb-8">
